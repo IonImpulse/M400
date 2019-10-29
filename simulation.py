8e5920a2e5738f109f01a4f4b17c4749de3847f2
Ethan <edv121@outlook.com>
2019-10-29 13:12:16 -0700

    initial list

---
+++ simulation.py
@@ -0,0 +1,15 @@
+#Simulation Requirements
+#Multiple Species
+#-Producer/Consumer (levels)
+
+#Pop Size
+#Max Age
+#Minumim/Maximum Reproduce Age
+
+#Order:
+#-Consume
+#-Effects
+#-Grow
+#-Reproduce
+#-Die
+#
